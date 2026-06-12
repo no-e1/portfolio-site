bc5fc323758e817ebfcd25a12ca8e00a1726e37e
no-e1 <133690936+no-e1@users.noreply.github.com>
2026-06-12 11:27:24 +0200

    feat(frontend): add footer content

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,20 @@
+import { FaGithub } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+
 export default function Footer() {
     return (
-        <h3>this is my footer</h3>
+        <footer>
+            <p className="footerText">© 2026 <a className="footerTextLink" href="/">xxx.xx</a> | <a id="footerTextLinkPrivacy" className="footerTextLink" href="/privacy">privacy</a></p>
+            
+            <div className="footerIcons">
+                <a href="https://github.com/no-e1" target="_blank">
+                    <FaGithub />
+                </a>
+
+                <a href="mailto:noelkohn@proton.me">
+                    <FaEnvelope />
+                </a>
+            </div>
+        </footer>
     );
 }

0a534f582ad31078c510e62edf6625bac93f637b
no-e1 <133690936+no-e1@users.noreply.github.com>
2026-08-14 15:12:30 +0200

    feat(homepage): change greetingscard-welcome-text

--- frontend/src/components/Home/GreetingsCard.tsx
+++ frontend/src/components/Home/GreetingsCard.tsx
@@ -7,8 +7,8 @@ export function GreetingsCard() {
         <article className={styles.home}>
             <h2 className={styles.subheader}>Lernender Informatiker Applikationsentwicklung EFZ</h2>
             <p className={styles.description}>
-                Willkommen auf meinem Portfolio. Hier findet man eine Übersicht über meine Projekte sowie
-                weitere Informationen über mich und meine Zeugnisse und Unterlagen.
+                Willkommen auf meiner persönlichen Website. Hier findet man eine Übersicht meiner Projekte sowie
+                weitere Informationen über mich. Bei Fragen und Anliegen bin ich hier erreichbar:
             </p>
             <ContactIcons showLabels />
             <FeaturedProjects />
